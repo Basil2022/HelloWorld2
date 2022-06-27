95ff298ce5a9e725f34ac184a1d9bce33e31c793
Amariei Vasile <vili_amariei@yahoo.com>
2022-06-27 20:25:35 +0300

    actualizat mesaj

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        System.out.println("Hello World");
+        System.out.println("Hello World, my name is Vasile");
         System.out.println("Hello World 2");
 	     System.out.println("Hello World 3");
 	     System.out.println("Hello World 4 ");
